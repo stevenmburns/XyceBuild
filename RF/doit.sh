c134d9c573b070ed448ada154fdb4b096523a598
Steven Burns <smburns47@gmail.com>
2019-04-25 11:19:13 -0700

    Add example circuits

---
+++ RF/doit.sh
@@ -0,0 +1,11 @@
+docker volume rm -f rfVol
+docker create --name for_adding_to_volume --mount source=rfVol,target=/circuits ubuntu
+docker cp cmosedu_models.txt for_adding_to_volume:/circuits
+docker cp RF.cir for_adding_to_volume:/circuits
+docker stop for_adding_to_volume
+docker rm for_adding_to_volume
+docker run --name xyce_container --mount source=rfVol,target=/circuits stevenmburns/xyce_small_ubuntu bash -c "cd circuits && Xyce RF.cir | tee LOG"
+docker cp xyce_container:/circuits .
+docker rm xyce_container
+
+gnuplot --persist p.gp
---
+++ RF/p.gp
@@ -0,0 +1,7 @@
+plot [:] [-0.1:1.1] \
+  '<head --lines=-1 circuits/RF.cir.prn' using 2:3 with lines, \
+  '<head --lines=-1 circuits/RF.cir.prn' using 2:4 with lines, \
+  '<head --lines=-1 circuits/RF.cir.prn' using 2:5 with lines, \
+  '<head --lines=-1 circuits/RF.cir.prn' using 2:6 with lines, \
+  '<head --lines=-1 circuits/RF.cir.prn' using 2:7 with lines, \
+  '<head --lines=-1 circuits/RF.cir.prn' using 2:8 with lines
